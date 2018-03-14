ec388e82972d891e49407b809e7d46bcd7e976c4
hwr <1092021277@qq.com>
2018-03-14 14:08:57 +0800

    [fix] 移除cors跨域解决方案

--- kuayu/server/node.js
+++ kuayu/server/node.js
@@ -7,9 +7,10 @@ const Koa = require('koa');
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
-const cors = require('@koa/cors');
 
-app.use(cors());
+//cors跨域解决方案
+// const cors = require('@koa/cors');
+// app.use(cors());
 
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
