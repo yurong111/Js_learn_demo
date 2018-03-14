8efb0d6e342c81f44d9975226ac6e78a2fed22d9
hwr <1092021277@qq.com>
2018-03-14 11:23:07 +0800

    [fix] 缺少声明koa

--- kuayu/server/node.js
+++ kuayu/server/node.js
@@ -6,9 +6,9 @@
 const Koa = require('koa');
 
 // 创建一个Koa对象表示web app本身:
-
+const app = new Koa();
 const cors = require('@koa/cors');
-// const app = new Koa();
+
 app.use(cors());
 
 // 对于任何请求，app将调用该异步函数处理请求：
